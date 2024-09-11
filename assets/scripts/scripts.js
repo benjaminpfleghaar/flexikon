5f0cd2ec08fc7a8bd844af865ea20cef27f554ea
Benjamin Pfleghaar <info@benjaminpfleghaar.de>
2024-09-11 10:59:33 +0200

    handle click event

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -2,8 +2,33 @@ const card = document.querySelector('[data-js="card"]');
 const question = document.querySelector('[data-js="question"]');
 const answer = document.querySelector('[data-js="answer"]');
 
-card &&
-	card.addEventListener("click", () => {
-		question.classList.toggle("question--hide");
-		answer.classList.toggle("answer--hide");
+const questionButton = document.querySelector('[data-js="question__button"]');
+const answerButton = document.querySelector('[data-js="answer__button"]');
+
+question &&
+	question.addEventListener("click", (e) => {
+		console.log(e.target, question);
+		if (e.target === question) {
+			question.classList.remove("question--hide");
+			answer.classList.add("answer--hide");
+		}
 	});
+
+answer &&
+	answer.addEventListener("click", (e) => {
+		console.log(e.target, answer);
+		if (e.target === answer) {
+			question.classList.add("question--hide");
+			answer.classList.remove("answer--hide");
+		}
+	});
+
+// const questionButton = document.querySelector('[data-js="question__button"]');
+// questionButton.addEventListener("click", () => {
+// 	questionButton.classList.toggle("question__button--active");
+// });
+
+// const answerButton = document.querySelector('[data-js="answer__button"]');
+// answerButton.addEventListener("click", () => {
+// 	answerButton.classList.toggle("answer__button--active");
+// });
