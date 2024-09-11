84d15a6b47fb430bad01c008d871b40bcd3062b0
Benjamin Pfleghaar <info@benjaminpfleghaar.de>
2024-09-11 10:03:56 +0200

    check if card exists

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -2,7 +2,8 @@ const card = document.querySelector('[data-js="card"]');
 const question = document.querySelector('[data-js="question"]');
 const answer = document.querySelector('[data-js="answer"]');
 
-card.addEventListener("click", () => {
-	question.classList.toggle("question--hide");
-	answer.classList.toggle("answer--hide");
-});
+card &&
+	card.addEventListener("click", () => {
+		question.classList.toggle("question--hide");
+		answer.classList.toggle("answer--hide");
+	});
