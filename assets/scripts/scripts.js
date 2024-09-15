fc4c8c2cf30b2cff26cf452747e0a28b4d976aab
Benjamin Pfleghaar <info@benjaminpfleghaar.de>
2024-09-15 19:17:08 +0200

    apply BEM naming convention

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,16 +3,16 @@ const cards = document.querySelectorAll('[data-js="card"]');
 [...cards].forEach((card, index) => {
 	const question = cards[index].querySelector('[data-js="question"]');
 	const answer = cards[index].querySelector('[data-js="answer"]');
-	const button = question.querySelector('[data-js="question__button"]');
+	const button = cards[index].querySelector('[data-js="button"]');
 
 	card.addEventListener("click", (e) => {
 		if (e.target !== button) {
-			question.classList.toggle("question--hidden");
-			answer.classList.toggle("answer--hidden");
+			question.classList.toggle("card__question--hidden");
+			answer.classList.toggle("card__answer--hidden");
 		}
 	});
 
 	button.addEventListener("click", () => {
-		button.classList.toggle("question__button--active");
+		button.classList.toggle("card__bookmark-button--active");
 	});
 });
